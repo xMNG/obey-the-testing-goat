a21bff4d28ffa287c65659e4b66b1ea118686d3a
MNG <ng.michael.m@gmail.com>
2018-11-06 16:42:35 -0500

    Refactored home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -6,11 +6,12 @@ from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
+    # def tearDown(self):
+    #     self.browser.quit()
 
     # first user story
     def test_can_start_a_list_and_retrieve_it_later(self):
--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,36 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from .views import home_page
+# from django.urls import resolve
+# from django.http import HttpRequest
+# from .views import home_page
 
 
 # Create your tests here.
 class HomePageTestCase(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+
+
+    # def test_root_url_resolves_to_home_page_view(self):
+    """
+    This test was rolled in the template check test
+    """
+        # found = resolve('/')
+        # self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  # take response
-        response = home_page(request)  # use response as argument to the view
-        html = response.content.decode('utf8')  # take the html response from the view and parse it
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        """
+        This is unwieldy, so replace with the self.client.get('/')
+        The old version created a http response and fed it to the view
+        The new version just feeds the url and checks for correct html output
+        """
+
+        # request = HttpRequest()  # take response
+        # response = home_page(request)  # use response as argument to the view
+        # html = response.content.decode('utf8')  # take the html response from the view and parse it
+        # self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.endswith('</html>'))
+
+        response = self.client.get('/')  # get the client response for this url
+        html = response.content.decode('utf8')  # get the html
+
+        self.assertTemplateUsed(response, 'html.html')
+

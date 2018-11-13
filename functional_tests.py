3405acfb6e97e3918c9be1b0016180f7d9d9116e
MNG <ng.michael.m@gmail.com>
2018-11-12 21:29:42 -0500

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,16 @@ class NewVisitorTest(unittest.TestCase):
     # def tearDown(self):
     #     self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """
+        Helper function for finding row text in table rows
+        :param row_text:
+        :return:
+        """
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     # first user story
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edit has heard about a cool new online to-do app. She goes to check out its homepage.
@@ -28,11 +38,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock features" into a text box (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys('Buy Peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists: "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "use peacock feathers to make a fly" (Edith is very methodical"
@@ -43,10 +54,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again, and now shows both items on her list
         # Edith wonders whether the site will remember her list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Item
 # from django.urls import resolve
 # from django.http import HttpRequest
 # from .views import home_page
@@ -6,39 +7,64 @@ from django.test import TestCase
 
 # Create your tests here.
 class HomePageTestCase(TestCase):
-
-    # def test_root_url_resolves_to_home_page_view(self):
     """
-    This test was rolled in the template check test
+    Test case for testing that the home page is uses correct template
     """
-    # found = resolve('/')
-    # self.assertEqual(found.func, home_page)
-
-    # def test_home_page_returns_correct_html(self):
-    """
-    This is unwieldy, so replace with the self.client.get('/')
-    The old version created a http response and fed it to the view
-    The new version just feeds the url and checks for correct html output
-    """
-
-    # request = HttpRequest()  # take response
-    # response = home_page(request)  # use response as argument to the view
-    # html = response.content.decode('utf8')  # take the html response from the view and parse it
-    # self.assertTrue(html.startswith('<!DOCTYPE html>'))
-    # self.assertIn('<title>To-Do lists</title>', html)
-    # self.assertTrue(html.endswith('</html>'))
-
-    # response = self.client.get('/')  # get the client response for this url
-    # html = response.content.decode('utf8')  # get the html
-    #
-    # self.assertTemplateUsed(response, 'home.html')
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_can_save_a_POST_request(self):
+        """
+        This tests whether the data from POST is saved to the HTML (DB later)
+        """
         # push this data to the post request
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """
+        This tests for redirect after POST request
+        """
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
+class ItemModelTestCase(TestCase):
+    """
+    This tests the model, not saving to the DB
+    """
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')

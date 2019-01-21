bcb8b65b5bef0775921339ec61098945f961c8a9
MNG <ng.michael.m@gmail.com>
2019-01-21 00:26:52 -0500

    fixed jquery for duplicate input error msg

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
 window.Superlists = {};
 window.Superlists.initialize = function() {
+    console.log('Initialized!');
     $('input[name="text"]').on('keypress', function () {
         $('.has-error').hide();
     });

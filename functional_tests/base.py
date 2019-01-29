32581e21daaa38eae1ca4d0ce6f11e3f5f757dc2
MNG <ng.michael.m@gmail.com>
2019-01-29 15:26:42 -0500

    debug false

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -29,38 +29,41 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait(function):
+        """
+        Decorator to wrap wait functions and isolate time features to this function
+        :return: wrapped function in wait
+        """
+        def modified_function(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return function(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_function
+
+    @wait
     def wait_for_row_in_list_table(self, row_text: str):
         """
         Helper function for finding row text in table rows
         :param row_text: text to search for
         :return: None
         """
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
+    @wait
     def wait_for(self, function_name):
         """
         Wait that pings the function periodically until timeout
         :param function_name: name of function, can be a lambda
         :return: function_name() result, or raises error
         """
-        start_time = time.time()
-        while True:
-            try:
-                return function_name()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return function_name()
 
     def get_item_input_box(self):
         """
@@ -71,19 +74,18 @@ class FunctionalTest(StaticLiveServerTestCase):
         return self.wait_for(lambda :self.browser.find_element_by_id('id_text'))
         # self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(member=email, container=navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(member=email, container=navbar.text)
+
 
 
 # removed because using django LiveTestCase runner to run these tests
